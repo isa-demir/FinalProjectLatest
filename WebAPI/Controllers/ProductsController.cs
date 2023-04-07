6f957a4046f861d03f7e2241b8bb6d3848d2a312
isademir <isademir.dev@gmail.com>
2023-04-07 03:06:56 +0300

    WebAPI eklendi ve ürün listeleme sağlandı.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return _productService.GetAll().Data;
+        }
+    }
+}
